5e180fef14d8846e8d170cdaf8ebf6963022d43b
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-08-17 15:34:52 +0100

    Configuration class instantiated through variable 'configuration', which is set to the property name "dfs.data.dir" and property value "data" (located in the librec.properties file in the conf folder)

--- src/recommender/system/lib/rec/java/project/RecommenderSystemDriverTest.java
+++ src/recommender/system/lib/rec/java/project/RecommenderSystemDriverTest.java
@@ -1,5 +1,20 @@
 package recommender.system.lib.rec.java.project;
 
-public class RecommenderSystemDriverTest {
+import net.librec.conf.Configuration;
+import net.librec.data.model.TextDataModel;
 
+public class RecommenderSystemDriverTest 
+{
+	public static void main(String[] args) throws Exception
+	{
+		Configuration configuration = new Configuration();
+		configuration.set("dfs.data.dir","data");
+		TextDataModel textDataModel = new TextDataModel(configuration);
+		textDataModel.buildDataModel();
+		
+		/*
+		 * To fix log4j:WARN error
+		 * https://stackoverflow.com/questions/12532339/no-appenders-could-be-found-for-loggerlog4j
+		 */
+	}
 }
