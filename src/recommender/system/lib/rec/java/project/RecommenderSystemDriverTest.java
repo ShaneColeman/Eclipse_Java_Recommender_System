48ec3c186ccd9b9858f4d413c13d6e8c67da072c
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-08-17 16:29:51 +0100

    RecommenderContext class instantiated though the 'recommenderContext' variable, which takes the variables 'configuration' and 'textDataModel' as method parameters

--- src/recommender/system/lib/rec/java/project/RecommenderSystemDriverTest.java
+++ src/recommender/system/lib/rec/java/project/RecommenderSystemDriverTest.java
@@ -2,19 +2,24 @@ package recommender.system.lib.rec.java.project;
 
 import net.librec.conf.Configuration;
 import net.librec.data.model.TextDataModel;
+import net.librec.recommender.RecommenderContext;
 
 public class RecommenderSystemDriverTest 
 {
 	public static void main(String[] args) throws Exception
 	{
+		//Building the Data Model (Text File Format)
 		Configuration configuration = new Configuration();
 		configuration.set("dfs.data.dir","data");
 		TextDataModel textDataModel = new TextDataModel(configuration);
 		textDataModel.buildDataModel();
 		
-		/*
+		 /*
 		 * To fix log4j:WARN error
 		 * https://stackoverflow.com/questions/12532339/no-appenders-could-be-found-for-loggerlog4j
 		 */
+		
+		//Building the Recommender System Context (Framework)
+		RecommenderContext recommenderContext = new RecommenderContext(configuration, textDataModel);
 	}
 }
