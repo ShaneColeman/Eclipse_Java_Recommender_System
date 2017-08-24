8286146abc92a069468aa669586798a28414fdca
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2017-08-24 17:43:42 +0100

    Code alterations made to 'RecommenderSystemDriverTest.java' file

--- src/recommender/system/lib/rec/java/project/RecommenderSystemDriverTest.java
+++ src/recommender/system/lib/rec/java/project/RecommenderSystemDriverTest.java
@@ -1,45 +1,61 @@
 package recommender.system.lib.rec.java.project;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import net.librec.conf.Configuration;
 import net.librec.data.model.TextDataModel;
 import net.librec.eval.RecommenderEvaluator;
+//import net.librec.eval.ranking.RecallEvaluator;
 import net.librec.eval.rating.RMSEEvaluator;
 import net.librec.filter.GenericRecommendedFilter;
 import net.librec.recommender.Recommender;
 import net.librec.recommender.RecommenderContext;
 import net.librec.recommender.baseline.ItemClusterRecommender;
-import net.librec.recommender.cf.ItemKNNRecommender;
-import net.librec.recommender.cf.UserKNNRecommender;
-import net.librec.recommender.cf.ranking.EALSRecommender;
+//import net.librec.recommender.cf.ItemKNNRecommender;
+//import net.librec.recommender.cf.UserKNNRecommender;
+//import net.librec.recommender.cf.ranking.EALSRecommender;
 import net.librec.recommender.item.RecommendedItem;
-import net.librec.similarity.BinaryCosineSimilarity;
-import net.librec.similarity.CosineSimilarity;
-import net.librec.similarity.JaccardSimilarity;
+//import net.librec.recommender.item.RecommendedList;
+//import net.librec.similarity.BinaryCosineSimilarity;
+//import net.librec.similarity.CosineSimilarity;
+//import net.librec.similarity.JaccardSimilarity;
 import net.librec.similarity.PCCSimilarity;
 import net.librec.similarity.RecommenderSimilarity;
 
 public class RecommenderSystemDriverTest 
 {
+	public static String CONFIGURATION_FILE = "conf/librec.properties";
+	
 	public static void main(String[] args) throws Exception
 	{
 		//Building the Data Model (Text File Format)
+		//Configuration configuration = new Configuration();
+		//configuration.set("dfs.data.dir","data");
+		
+		//Building the Data Model (Text File Format) TEST CODE
+		String configurationFilePath = CONFIGURATION_FILE;
 		Configuration configuration = new Configuration();
-		configuration.set("dfs.data.dir","data");
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(configurationFilePath));
+		for(String name: properties.stringPropertyNames())
+		{
+			configuration.set(name, properties.getProperty(name));
+		}
+		
 		TextDataModel textDataModel = new TextDataModel(configuration);
 		textDataModel.buildDataModel();
 		
-		 /*
+		/*
 		 * To fix log4j:WARN error
 		 * https://stackoverflow.com/questions/12532339/no-appenders-could-be-found-for-loggerlog4j
 		 */
 		
 		//Building the Recommender System Context (Framework)
 		RecommenderContext recommenderContext = new RecommenderContext(configuration, textDataModel);
-		
-		
+
 		//Building the Recommender System Similarity 
 		//configuration.set("rec.recommender.similarity.key", "item");
 		RecommenderSimilarity recommenderSimilarity = new PCCSimilarity();
@@ -49,6 +65,7 @@ public class RecommenderSystemDriverTest
 		//Building the Recommender System Model
 		//configuration.set("rec.neighbours.knn.number", "5");
 		Recommender recommender = new ItemClusterRecommender();
+		//Recommender recommender = new ItemKNNRecommender();
 		recommender.setContext(recommenderContext);
 		
 		//Executing (running) the Recommender System Model (Algorithm)
@@ -56,15 +73,14 @@ public class RecommenderSystemDriverTest
 		
 		//Evaluating the Recommender System Model (Algorithm) Results
 		RecommenderEvaluator recommenderEvaluator = new RMSEEvaluator();
-		System.out.println("Root Mean Square Error: " + recommender.evaluate(recommenderEvaluator));
-		
-		
+		System.out.println("Root Mean Square Error (RMSE): " + recommender.evaluate(recommenderEvaluator));
+
 		//Set ID List (User and Item) of Filter
 		List<String> userIDList = new ArrayList<>();
 		List<String> itemIDList = new ArrayList<>();
-		userIDList.add("CVE-2017-0001");
-		itemIDList.add("CWE-20");
-		
+		userIDList.add("CVE");
+		//userIDList.add("CVE-2017-0001");
+		itemIDList.add("CWE-264");
 		
 		//Filtering the Recommender System Recommended Results
 		List<RecommendedItem> recommendedItemList = recommender.getRecommendedList();
@@ -76,11 +92,10 @@ public class RecommenderSystemDriverTest
 		//Printing the Recommender System Recommended Results (Filtered)
 		for(RecommendedItem recommendedItem : recommendedItemList)
 		{
-			System.out.println("User: " + recommendedItem.getUserId() + " " +
-								"Item: " + recommendedItem.getItemId() + " " +
+			System.out.println("User (CVE ID): " + recommendedItem.getUserId() + " - " +
+								"Item (CWE): " + recommendedItem.getItemId() + " - " +
 								"Value: " + recommendedItem.getValue());
 		}
-		
-		
+
 	}
 }
